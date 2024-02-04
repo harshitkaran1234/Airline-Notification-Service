95f4bb94b49679a0f7a5dc79dd4936606f611e74
karanharshit <harsheet2020@gmail.com>
2024-02-04 17:32:51 +0530

    sent mail using nodemailer package

--- src/index.js
+++ src/index.js
@@ -4,9 +4,25 @@ const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
 
 const app = express();
+const mailSender = require('./config/email-config');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.use('/api', apiRoutes);
 
-app.listen(ServerConfig.PORT, () => {
+app.listen(ServerConfig.PORT, async () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+    try{
+        const response = await mailSender.sendMail({
+            from: ServerConfig.GMAIL_EMAIL,
+            to: 'harsheet.new@gmail.com',
+            subject: 'is the service working',
+            text: 'Yes, it is working',
+        })
+        console.log(response);
+    } catch(error) {
+        console.log(error);
+    }
+
 });
